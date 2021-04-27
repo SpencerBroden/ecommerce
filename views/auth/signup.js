579eae0e6b3cd3f571157484a52bde03b9af2a9a
SpencerBroden <9060485+SpencerBroden@users.noreply.github.com>
2021-04-26 20:06:10 -0700

    24

--- views/auth/signup.js
+++ views/auth/signup.js
@@ -1,37 +1,4 @@
 const layout = require('../layout');
-const { getError } = require('../helpers');
-
-const register = () => {
-  if (validateEmail(registerEmail) === false) {
-    alert('Please enter a valid email address.');
-  } else if (validatePassword(registerPassword) === false) {
-    alert(
-      'Passwords must contain at least one uppercase letter, one lowercase letter, and need to be at least 8 characters long.'
-    );
-  } else if (registerPassword.localeCompare(repeatPassword) !== 0) {
-    alert('Passwords should match.');
-  } else if (check === false) {
-    alert('Please agree to the terms and conditions.');
-  } else {
-    Axios.post('https://ecs-cheng-alvis-api.herokuapp.com/email', {
-      email: registerEmail,
-    }).then((response) => {
-      //console.log(String(response.data));
-      if (String(response.data) === 'false') {
-        Axios.post('https://ecs-cheng-alvis-api.herokuapp.com/register', {
-          email: email,
-          password: password,
-        }).then((response) => {
-          //console.log(response);
-        });
-      } else {
-        //console.log(String(response.data));
-        alert('This email is already in use.');
-      }
-    });
-  }
-};
-
 module.exports = ({ req, errors }) => {
   return layout({
     content: `
@@ -43,20 +10,17 @@ module.exports = ({ req, errors }) => {
               <div class="field">
                 <label class="label">Email</label>
                 <input required class="input" placeholder="Email" name="email" />
-                <p class="help is-danger">${getError(errors, 'email')}</p>
+                <p class="help is-danger"></p>
               </div>
               <div class="field">
                 <label class="label">Password</label>
                 <input required class="input" placeholder="Password" name="password" type="password" />
-                <p class="help is-danger">${getError(errors, 'password')}</p>
+                <p class="help is-danger"></p>
               </div>
               <div class="field">
                 <label class="label">Password Confirmation</label>
                 <input required class="input" placeholder="Password Confirmation" name="passwordConfirmation" type="password" />
-                <p class="help is-danger">${getError(
-                  errors,
-                  'passwordConfirmation'
-                )}</p>
+                <p class="help is-danger"></p>
               </div>
               <button class="button is-primary">Submit</button>
             </form>
