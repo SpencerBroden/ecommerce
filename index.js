dcfbc4186a2e8c4d0048df514ee0aca2597069cc
SpencerBroden <9060485+SpencerBroden@users.noreply.github.com>
2021-04-26 19:43:28 -0700

    6

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const storeTemplate = require('./views/store/index');
 const signupTemplate = require('./views/auth/signup');
 const db = require('./model');
