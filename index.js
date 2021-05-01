f49420aa1f7c7e4a91030840fcbbf613aba0885f
SpencerBroden <9060485+SpencerBroden@users.noreply.github.com>
2021-04-30 21:43:40 -0700

    product

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ app.use(express.json());
 app.use(cors());
 app.use(authRouter);
 
+app.get('/', async (req, res) => {
+  const products = await db.getProducts;
+  res.send(storeTemplate({ products }));
+});
+
 app.get('/api/users', db.getUsers);
 
 app.get('/api/hashs', db.getLogins);
@@ -18,13 +23,6 @@ app.get('/api/hashs', db.getLogins);
 app.get('/api/products', db.getProducts);
 
 app.get('/api/orders', db.getOrders);
-
-app.get('/', async (req, res) => {
-  fetch(
-    'https://csc174-ecommerce-project.herokuapp.com/api/products'
-  ).then((response) => response.json());
-  res.send(storeTemplate({ response }));
-});
 
 app.listen(process.env.PORT || 4000, () => {
   console.log(`app is running on port ${process.env.PORT || 4000}`);
