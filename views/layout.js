24289f47d812ca2364d807aa21b6e79616b98b96
SpencerBroden <9060485+SpencerBroden@users.noreply.github.com>
2021-05-01 16:21:41 -0700

    order

--- views/layout.js
+++ views/layout.js
@@ -9,7 +9,6 @@ module.exports = ({ content }) => {
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" rel="stylesheet">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css"></link>
       </head>
-
       <body>
         <header>
           <nav class="navbar navbar-bottom">
